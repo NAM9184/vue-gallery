513e03e54196fbfea8f1355970d65aa551b2bf93
ruelline <ruelline91@naver.com>
2025-04-01 14:45:02 +0900

    [feat] modal mouseEvent 추가 조명이펙트 생성

--- src/components/ImageModal.vue
+++ src/components/ImageModal.vue
@@ -1,19 +1,46 @@
 <template>
   <div class="modal" v-if="image" @click.self="$emit('close')">
-    <div class="modal-content" ref="modal" @mousemove="handleMouseMove">
+    <div
+      class="modal-content"
+      ref="modal"
+      @mousemove="handleMouseMove"
+      @mouseleave="resetLight"
+    >
       <img :src="image.src" />
-      <p>{{ image.title }}</p>
+      <div class="dark-overlay" :style="lightMaskStyle"></div>
+
+      <p class="title">{{ image.title }}</p>
     </div>
   </div>
 </template>
 
-<script>
-export default {
-  name: "ImageModal",
-  props: {
-    image: Object,
-  },
+<script setup>
+import { computed, ref } from "vue";
+const props = defineProps({
+  image: Object,
+});
+const emit = defineEmits(["close"]);
+
+const modal = ref(null);
+const lightX = ref(0);
+const lightY = ref(0);
+
+const showLight = ref(false);
+
+const handleMouseMove = (e) => {
+  const rect = modal.value.getBoundingClientRect();
+  const x = e.clientX - rect.left;
+  const y = e.clientY - rect.top;
+  lightX.value = x;
+  lightY.value = y;
+
+  lightMaskStyle.value = {
+    "--x": `${x}px`,
+    "--y": `${y}px`,
+  };
 };
+
+const lightMaskStyle = ref({});
 </script>
 
 <style scoped>
@@ -28,9 +55,43 @@ export default {
   justify-content: center;
   align-items: center;
 }
-.modal-content img {
+
+.modal-content {
+  position: relative;
   max-width: 90vw;
   max-height: 80vh;
   border-radius: 8px;
+  overflow: hidden;
+}
+
+.modal-content img {
+  width: 100%;
+  display: block;
+}
+
+.title {
+  font-size: 5rem;
+}
+
+.dark-overlay {
+  position: absolute;
+  inset: 0;
+  background: #000000; /* 전체 어둡게 */
+  pointer-events: none;
+  z-index: 10;
+
+  /* 조명처럼 비추기 위해 마스크 사용 */
+  -webkit-mask-image: radial-gradient(
+    circle 200px at var(--x, 50%) var(--y, 50%),
+    transparent 0%,
+    black 100%
+  );
+  mask-image: radial-gradient(
+    circle 200px at var(--x, 50%) var(--y, 50%),
+    transparent 0%,
+    black 100%
+  );
+  mask-composite: exclude;
+  -webkit-mask-composite: destination-out;
 }
 </style>
